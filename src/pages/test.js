698a79afe83f657fe92874a51e7984f0aab24c67
woominsik <9737638@naver.com>
2022-06-28 10:10:48 +0900

    세팅

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function AboutPage(){
+    return(
+        <main>
+            <title>자기소개</title>
+            <h1>자기 소개 페이지입니다.</h1>
+        </main>
+    );
+}
+
+export default AboutPage;
